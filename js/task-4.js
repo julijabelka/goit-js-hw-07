af3219dba6458f2848217d13fa923739b3ef0252
julijabelka <julija.belka@gmail.com>
2024-03-01 11:57:55 +0100

    fix task4

--- js/task-4.js
+++ js/task-4.js
@@ -32,7 +32,7 @@ form.addEventListener('submit', handleSubmit);
 function handleSubmit (event) {
     event.preventDefault();
     const emailUser = event.currentTarget.elements.email.value.trim();
-    const passwordUser = event.currentTarget.elements.password.value.tri();
+    const passwordUser = event.currentTarget.elements.password.value.trim();
     
     if (emailUser === "" || passwordUser === "")  {
         alert ('All form fields must be filled in');
